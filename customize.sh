9d1e9c17728b926962c259478d8aaebfeda83a5d
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-05-22 21:58:26 +0700

    v2.3

--- customize.sh
+++ customize.sh
@@ -29,6 +29,13 @@ if [ "`grep_prop debug.log $OPTIONALS`" == 1 ]; then
   ui_print " "
 fi
 
+# recovery
+if [ "$BOOTMODE" != true ]; then
+  MODPATH_UPDATE=`echo $MODPATH | sed 's|modules/|modules_update/|g'`
+  rm -f $MODPATH/update
+  rm -rf $MODPATH_UPDATE
+fi
+
 # run
 . $MODPATH/function.sh
 
@@ -849,6 +856,14 @@ if echo "$PROP" | grep -q g; then
   sed -i 's|#g||g' $FILE
   ui_print " "
 fi
+if [ $DOLBY == true ]; then
+  if [ "`grep_prop dolby.game $OPTIONALS`" != 0 ]; then
+    sed -i 's|#x||g' $FILE
+  else
+    ui_print "- Does not use Dolby Game patch & rerouting stream"
+    ui_print " "
+  fi
+fi
 
 # function
 dolby_settings() {
@@ -1095,7 +1110,7 @@ fi
 # raw
 FILE=$MODPATH/.aml.sh
 if [ "`grep_prop disable.raw $OPTIONALS`" == 0 ]; then
-  ui_print "- Not disables Ultra Low Latency playback (RAW)"
+  ui_print "- Does not disable Ultra Low Latency playback (RAW)"
   ui_print " "
 else
   sed -i 's|#u||g' $FILE
